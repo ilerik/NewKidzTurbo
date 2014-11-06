fe8c18cbaddf5aa9439f234c435245d3a198417d
ilerik <erik.lite@gmail.com>
2014-11-06 03:42:23 +0300

    Erik. New RiemannSolver base class, HLLC solver implemented for general EOS.
    1) Also signum function and new file utilityfunctions.h for storing utility functions

---
+++ src/utility/utilityfunctions.h
@@ -0,0 +1,9 @@
+#ifndef TURBO_UtilityFUNCTIONS
+#define TURBO_UtilityFUNCTIONS
+
+//Signum function from http://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+template <typename T> int sign(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+#endif
